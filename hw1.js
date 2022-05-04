b24737225fcdb92bfbe654460756a5d2d9a079e8
ayazimahsa <mahsaayazii73@gmail.com>
2022-05-05 03:00:00 +0430

    add hw2

--- hw1.js
+++
@@ -1,8 +0,0 @@
-function accumulatingArray(array) {
-    let sum = (sum => value => sum += value)(0);
-    return array.map(sum);
-}
-console.log(accumulatingArray([1, 2, 3, 4]));
-console.log(accumulatingArray([1, 5, 7]));
-console.log(accumulatingArray([1, 0, 1, 0, 1]));
-console.log(accumulatingArray([]));
